795f00eee0e385d7479e66501b1439cb00749768
rangke <c14180243@john.petra.ac.id>
2021-03-17 10:34:09 +0800

    AboutUSDone

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,27 @@ export class Tab2Page {
 
   constructor() {}
 
+  random = 0;
+  answer = false;
+  answerangka : number;
+  guide = "";
+
+  ngOnInit(){
+    this.random = Math.floor(Math.random() * (10 - 1) + 1);
+    this.answer = false;
+  }
+
+  cekangka(){
+    if (this.answerangka == this.random){
+      this.answer = true;
+      this.guide = "Tebakan Betul";
+    } 
+    else if (this.answerangka > this.random){
+      this.guide = "Angka Terlalu Besar";
+    }
+    else if (this.answerangka < this.random){
+      this.guide = "Angka Terlalu Kecil";
+    }
+  }
+
 }
